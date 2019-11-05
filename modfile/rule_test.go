c90efee705eefc6efa975842f5e85f455b0b4639
Bryan C. Mills <bcmills@google.com>
2019-11-05 13:18:05 -0500

    modfile: prefer to add a new 'go' directive after the 'module' directive
    The previous behavior put the new 'go' directive at the end of the
    file, which is not where anybody actually puts it, and that in turn
    complicates the regression tests for golang/go#34822.
    
    Updates golang/go#34822
    
    Change-Id: Ic275eb6999fc0db7e72e2d9636b77c9e3d919539
    Reviewed-on: https://go-review.googlesource.com/c/mod/+/205497
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Jay Conrod <jayconrod@google.com>

--- modfile/rule_test.go
+++ modfile/rule_test.go
@@ -96,6 +96,47 @@ var setRequireTests = []struct {
 	},
 }
 
+var addGoTests = []struct {
+	in      string
+	version string
+	out     string
+}{
+	{`module m
+		`,
+		`1.14`,
+		`module m
+		go 1.14
+		`,
+	},
+	{`module m
+		require x.y/a v1.2.3
+		`,
+		`1.14`,
+		`module m
+		go 1.14
+		require x.y/a v1.2.3
+		`,
+	},
+	{
+		`require x.y/a v1.2.3
+		module example.com/inverted
+		`,
+		`1.14`,
+		`require x.y/a v1.2.3
+		module example.com/inverted
+		go 1.14
+		`,
+	},
+	{
+		`require x.y/a v1.2.3
+		`,
+		`1.14`,
+		`require x.y/a v1.2.3
+		go 1.14
+		`,
+	},
+}
+
 func TestAddRequire(t *testing.T) {
 	for i, tt := range addRequireTests {
 		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
@@ -163,6 +204,36 @@ func TestSetRequire(t *testing.T) {
 			f.Cleanup()
 			if len(f.Require) != len(mods) {
 				t.Errorf("after Cleanup, len(Require) = %v; want %v", len(f.Require), len(mods))
+			}
+		})
+	}
+}
+
+func TestAddGo(t *testing.T) {
+	for i, tt := range addGoTests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			f, err := Parse("in", []byte(tt.in), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			g, err := Parse("out", []byte(tt.out), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			golden, err := g.Format()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if err := f.AddGoStmt(tt.version); err != nil {
+				t.Fatal(err)
+			}
+			out, err := f.Format()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Equal(out, golden) {
+				t.Errorf("have:\n%s\nwant:\n%s", out, golden)
 			}
 		})
 	}
