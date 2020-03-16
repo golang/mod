c0d644d00ab849f4506f17a98a5740bf0feff020
Jay Conrod <jayconrod@google.com>
2020-03-16 17:44:38 -0400

    modfile: support retract directive and version intervals
    This CL adds support for parsing and programmatically adding and
    removing a new directive, "retract", as described in golang/go#24031.
    
    The "retract" directive comes in two forms:
    
        retract v1.0.0           // single version
        retract [v1.1.0, v1.2.0] // closed interval
    
    Updates golang/go#24031
    
    Change-Id: I1236c7d89e7674abf694e49e9b4869b14a59fac0
    Reviewed-on: https://go-review.googlesource.com/c/mod/+/228039
    Run-TryBot: Jay Conrod <jayconrod@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Michael Matloob <matloob@golang.org>

--- modfile/rule_test.go
+++ modfile/rule_test.go
@@ -6,19 +6,20 @@ package modfile
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"golang.org/x/mod/module"
 )
 
 var addRequireTests = []struct {
+	desc string
 	in   string
 	path string
 	vers string
 	out  string
 }{
 	{
+		`existing`,
 		`
 		module m
 		require x.y/z v1.2.3
@@ -30,6 +31,7 @@ var addRequireTests = []struct {
 		`,
 	},
 	{
+		`new`,
 		`
 		module m
 		require x.y/z v1.2.3
@@ -44,6 +46,7 @@ var addRequireTests = []struct {
 		`,
 	},
 	{
+		`new2`,
 		`
 		module m
 		require x.y/z v1.2.3
@@ -62,6 +65,7 @@ var addRequireTests = []struct {
 }
 
 var setRequireTests = []struct {
+	desc string
 	in   string
 	mods []struct {
 		path     string
@@ -71,6 +75,7 @@ var setRequireTests = []struct {
 	out string
 }{
 	{
+		`existing`,
 		`module m
 		require (
 			x.y/b v1.2.3
@@ -97,6 +102,7 @@ var setRequireTests = []struct {
 		`,
 	},
 	{
+		`existing_indirect`,
 		`module m
 		require (
 			x.y/a v1.2.3
@@ -136,18 +142,23 @@ var setRequireTests = []struct {
 }
 
 var addGoTests = []struct {
+	desc    string
 	in      string
 	version string
 	out     string
 }{
-	{`module m
+	{
+		`module_only`,
+		`module m
 		`,
 		`1.14`,
 		`module m
 		go 1.14
 		`,
 	},
-	{`module m
+	{
+		`module_before_require`,
+		`module m
 		require x.y/a v1.2.3
 		`,
 		`1.14`,
@@ -157,6 +168,7 @@ var addGoTests = []struct {
 		`,
 	},
 	{
+		`require_before_module`,
 		`require x.y/a v1.2.3
 		module example.com/inverted
 		`,
@@ -167,6 +179,7 @@ var addGoTests = []struct {
 		`,
 	},
 	{
+		`require_only`,
 		`require x.y/a v1.2.3
 		`,
 		`1.14`,
@@ -176,51 +189,398 @@ var addGoTests = []struct {
 	},
 }
 
-func TestAddRequire(t *testing.T) {
-	for i, tt := range addRequireTests {
-		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
-			f, err := Parse("in", []byte(tt.in), nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			g, err := Parse("out", []byte(tt.out), nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			golden, err := g.Format()
-			if err != nil {
-				t.Fatal(err)
-			}
+var addRetractTests = []struct {
+	desc      string
+	in        string
+	low       string
+	high      string
+	rationale string
+	out       string
+}{
+	{
+		`new_singleton`,
+		`module m
+		`,
+		`v1.2.3`,
+		`v1.2.3`,
+		``,
+		`module m
+		retract v1.2.3
+		`,
+	},
+	{
+		`new_interval`,
+		`module m
+		`,
+		`v1.0.0`,
+		`v1.1.0`,
+		``,
+		`module m
+		retract [v1.0.0, v1.1.0]`,
+	},
+	{
+		`duplicate_with_rationale`,
+		`module m
+		retract v1.2.3
+		`,
+		`v1.2.3`,
+		`v1.2.3`,
+		`bad`,
+		`module m
+		retract (
+			v1.2.3
+			// bad
+			v1.2.3
+		)
+		`,
+	},
+	{
+		`duplicate_multiline_rationale`,
+		`module m
+		retract [v1.2.3, v1.2.3]
+		`,
+		`v1.2.3`,
+		`v1.2.3`,
+		`multi
+line`,
+		`module m
+		retract	(
+			[v1.2.3, v1.2.3]
+			// multi
+			// line
+			v1.2.3
+		)
+		`,
+	},
+	{
+		`duplicate_interval`,
+		`module m
+		retract [v1.0.0, v1.1.0]
+		`,
+		`v1.0.0`,
+		`v1.1.0`,
+		``,
+		`module m
+		retract (
+			[v1.0.0, v1.1.0]
+			[v1.0.0, v1.1.0]
+		)
+		`,
+	},
+	{
+		`duplicate_singleton`,
+		`module m
+		retract v1.2.3
+		`,
+		`v1.2.3`,
+		`v1.2.3`,
+		``,
+		`module m
+		retract	(
+			v1.2.3
+			v1.2.3
+		)
+		`,
+	},
+}
 
-			if err := f.AddRequire(tt.path, tt.vers); err != nil {
-				t.Fatal(err)
-			}
-			out, err := f.Format()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if !bytes.Equal(out, golden) {
-				t.Errorf("have:\n%s\nwant:\n%s", out, golden)
-			}
+var dropRetractTests = []struct {
+	desc string
+	in   string
+	low  string
+	high string
+	out  string
+}{
+	{
+		`singleton_no_match`,
+		`module m
+		retract v1.2.3
+		`,
+		`v1.0.0`,
+		`v1.0.0`,
+		`module m
+		retract v1.2.3
+		`,
+	},
+	{
+		`singleton_match_one`,
+		`module m
+		retract v1.2.2
+		retract v1.2.3
+		retract v1.2.4
+		`,
+		`v1.2.3`,
+		`v1.2.3`,
+		`module m
+		retract v1.2.2
+		retract v1.2.4
+		`,
+	},
+	{
+		`singleton_match_all`,
+		`module m
+		retract v1.2.3 // first
+		retract v1.2.3 // second
+		`,
+		`v1.2.3`,
+		`v1.2.3`,
+		`module m
+		`,
+	},
+	{
+		`interval_match`,
+		`module m
+		retract [v1.2.3, v1.2.3]
+		`,
+		`v1.2.3`,
+		`v1.2.3`,
+		`module m
+		`,
+	},
+	{
+		`interval_superset_no_match`,
+		`module m
+		retract [v1.0.0, v1.1.0]
+		`,
+		`v1.0.0`,
+		`v1.2.0`,
+		`module m
+		retract [v1.0.0, v1.1.0]
+		`,
+	},
+	{
+		`singleton_match_middle`,
+		`module m
+		retract v1.2.3
+		`,
+		`v1.2.3`,
+		`v1.2.3`,
+		`module m
+		`,
+	},
+	{
+		`interval_match_middle_block`,
+		`module m
+		retract (
+			v1.0.0
+			[v1.1.0, v1.2.0]
+			v1.3.0
+		)
+		`,
+		`v1.1.0`,
+		`v1.2.0`,
+		`module m
+		retract (
+			v1.0.0
+			v1.3.0
+		)
+		`,
+	},
+	{
+		`interval_match_all`,
+		`module m
+		retract [v1.0.0, v1.1.0]
+		retract [v1.0.0, v1.1.0]
+		`,
+		`v1.0.0`,
+		`v1.1.0`,
+		`module m
+		`,
+	},
+}
+
+var retractRationaleTests = []struct {
+	desc, in, want string
+}{
+	{
+		`no_comment`,
+		`module m
+		retract v1.0.0`,
+		``,
+	},
+	{
+		`prefix_one`,
+		`module m
+		//   prefix
+		retract v1.0.0 
+		`,
+		`prefix`,
+	},
+	{
+		`prefix_multiline`,
+		`module m
+		//  one
+		//
+		//     two
+		//
+		// three  
+		retract v1.0.0`,
+		`one
+
+two
+
+three`,
+	},
+	{
+		`suffix`,
+		`module m
+		retract v1.0.0 // suffix
+		`,
+		`suffix`,
+	},
+	{
+		`prefix_suffix_after`,
+		`module m
+		// prefix
+		retract v1.0.0 // suffix
+		`,
+		`prefix
+suffix`,
+	},
+	{
+		`block_only`,
+		`// block
+		retract (
+			v1.0.0
+		)
+		`,
+		`block`,
+	},
+	{
+		`block_and_line`,
+		`// block
+		retract (
+			// line
+			v1.0.0
+		)
+		`,
+		`line`,
+	},
+}
+
+var sortBlocksTests = []struct {
+	desc, in, out string
+	strict        bool
+}{
+	{
+		`exclude_duplicates_removed`,
+		`module m
+		exclude x.y/z v1.0.0 // a
+		exclude x.y/z v1.0.0 // b
+		exclude (
+			x.y/w v1.1.0
+			x.y/z v1.0.0 // c
+		)
+		`,
+		`module m
+		exclude x.y/z v1.0.0 // a
+		exclude (
+			x.y/w v1.1.0
+		)`,
+		true,
+	},
+	{
+		`replace_duplicates_removed`,
+		`module m
+		replace x.y/z v1.0.0 => ./a
+		replace x.y/z v1.1.0 => ./b
+		replace (
+			x.y/z v1.0.0 => ./c
+		)
+		`,
+		`module m
+		replace x.y/z v1.1.0 => ./b
+		replace (
+			x.y/z v1.0.0 => ./c
+		)
+		`,
+		true,
+	},
+	{
+		`retract_duplicates_not_removed`,
+		`module m
+		// block
+		retract (
+			v1.0.0 // one
+			v1.0.0 // two
+		)`,
+		`module m
+		// block
+		retract (
+			v1.0.0 // one
+			v1.0.0 // two
+		)`,
+		true,
+	},
+	// Tests below this point just check sort order.
+	// Non-retract blocks are sorted lexicographically in ascending order.
+	// retract blocks are sorted using semver in descending order.
+	{
+		`sort_lexicographically`,
+		`module m
+		sort (
+			aa
+			cc
+			bb
+			zz
+			v1.2.0
+			v1.11.0
+		)`,
+		`module m
+		sort (
+			aa
+			bb
+			cc
+			v1.11.0
+			v1.2.0
+			zz
+		)
+		`,
+		false,
+	},
+	{
+		`sort_retract`,
+		`module m
+		retract (
+			[v1.2.0, v1.3.0]
+			[v1.1.0, v1.3.0]
+			[v1.1.0, v1.2.0]
+			v1.0.0
+			v1.1.0
+			v1.2.0
+			v1.3.0
+			v1.4.0
+		)
+		`,
+		`module m
+		retract (
+			v1.4.0
+			v1.3.0
+			[v1.2.0, v1.3.0]
+			v1.2.0
+			[v1.1.0, v1.3.0]
+			[v1.1.0, v1.2.0]
+			v1.1.0
+			v1.0.0
+		)
+		`,
+		false,
+	},
+}
+
+func TestAddRequire(t *testing.T) {
+	for _, tt := range addRequireTests {
+		t.Run(tt.desc, func(t *testing.T) {
+			testEdit(t, tt.in, tt.out, true, func(f *File) error {
+				return f.AddRequire(tt.path, tt.vers)
+			})
 		})
 	}
 }
 
 func TestSetRequire(t *testing.T) {
-	for i, tt := range setRequireTests {
-		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
-			f, err := Parse("in", []byte(tt.in), nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			g, err := Parse("out", []byte(tt.out), nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			golden, err := g.Format()
-			if err != nil {
-				t.Fatal(err)
-			}
+	for _, tt := range setRequireTests {
+		t.Run(tt.desc, func(t *testing.T) {
 			var mods []*Require
 			for _, mod := range tt.mods {
 				mods = append(mods, &Require{
@@ -232,14 +592,10 @@ func TestSetRequire(t *testing.T) {
 				})
 			}
 
-			f.SetRequire(mods)
-			out, err := f.Format()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if !bytes.Equal(out, golden) {
-				t.Errorf("have:\n%s\nwant:\n%s", out, golden)
-			}
+			f := testEdit(t, tt.in, tt.out, true, func(f *File) error {
+				f.SetRequire(mods)
+				return nil
+			})
 
 			f.Cleanup()
 			if len(f.Require) != len(mods) {
@@ -250,31 +606,96 @@ func TestSetRequire(t *testing.T) {
 }
 
 func TestAddGo(t *testing.T) {
-	for i, tt := range addGoTests {
-		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+	for _, tt := range addGoTests {
+		t.Run(tt.desc, func(t *testing.T) {
+			testEdit(t, tt.in, tt.out, true, func(f *File) error {
+				return f.AddGoStmt(tt.version)
+			})
+		})
+	}
+}
+
+func TestAddRetract(t *testing.T) {
+	for _, tt := range addRetractTests {
+		t.Run(tt.desc, func(t *testing.T) {
+			testEdit(t, tt.in, tt.out, true, func(f *File) error {
+				return f.AddRetract(VersionInterval{Low: tt.low, High: tt.high}, tt.rationale)
+			})
+		})
+	}
+}
+
+func TestDropRetract(t *testing.T) {
+	for _, tt := range dropRetractTests {
+		t.Run(tt.desc, func(t *testing.T) {
+			testEdit(t, tt.in, tt.out, true, func(f *File) error {
+				if err := f.DropRetract(VersionInterval{Low: tt.low, High: tt.high}); err != nil {
+					return err
+				}
+				f.Cleanup()
+				return nil
+			})
+		})
+	}
+}
+
+func TestRetractRationale(t *testing.T) {
+	for _, tt := range retractRationaleTests {
+		t.Run(tt.desc, func(t *testing.T) {
 			f, err := Parse("in", []byte(tt.in), nil)
 			if err != nil {
 				t.Fatal(err)
 			}
-			g, err := Parse("out", []byte(tt.out), nil)
-			if err != nil {
-				t.Fatal(err)
+			if len(f.Retract) != 1 {
+				t.Fatalf("got %d retract directives; want 1", len(f.Retract))
 			}
-			golden, err := g.Format()
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if err := f.AddGoStmt(tt.version); err != nil {
-				t.Fatal(err)
-			}
-			out, err := f.Format()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if !bytes.Equal(out, golden) {
-				t.Errorf("have:\n%s\nwant:\n%s", out, golden)
+			if got := f.Retract[0].Rationale; got != tt.want {
+				t.Errorf("got %q; want %q", got, tt.want)
 			}
 		})
 	}
+}
+
+func TestSortBlocks(t *testing.T) {
+	for _, tt := range sortBlocksTests {
+		t.Run(tt.desc, func(t *testing.T) {
+			testEdit(t, tt.in, tt.out, tt.strict, func(f *File) error {
+				f.SortBlocks()
+				return nil
+			})
+		})
+	}
+}
+
+func testEdit(t *testing.T, in, want string, strict bool, transform func(f *File) error) *File {
+	t.Helper()
+	parse := Parse
+	if !strict {
+		parse = ParseLax
+	}
+	f, err := parse("in", []byte(in), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	g, err := parse("out", []byte(want), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	golden, err := g.Format()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := transform(f); err != nil {
+		t.Fatal(err)
+	}
+	out, err := f.Format()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(out, golden) {
+		t.Errorf("have:\n%s\nwant:\n%s", out, golden)
+	}
+
+	return f
 }
