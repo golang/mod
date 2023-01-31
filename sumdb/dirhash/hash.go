b71060237c896c7c9fc602ab66e33ea6079659fa
Russ Cox <rsc@golang.org>
2023-01-31 09:28:07 -0500

    sumdb/dirhash: correct documentation of hash
    The "equivalent Unix command" was missing a layer of sha256sum.
    
    Fixes golang/go#48498.
    
    Change-Id: I1051bfa5fcbbf1625623d11cf21686b33da19b88
    Reviewed-on: https://go-review.googlesource.com/c/mod/+/464295
    Auto-Submit: Russ Cox <rsc@golang.org>
    Reviewed-by: Bryan Mills <bcmills@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>

--- sumdb/dirhash/hash.go
+++ sumdb/dirhash/hash.go
@@ -33,7 +33,7 @@ type Hash func(files []string, open func(string) (io.ReadCloser, error)) (string
 // Hash1 is "h1:" followed by the base64-encoded SHA-256 hash of a summary
 // prepared as if by the Unix command:
 //
-//	find . -type f | sort | sha256sum
+//	sha256sum $(find . -type f | sort) | sha256sum
 //
 // More precisely, the hashed summary contains a single line for each file in the list,
 // ordered by sort.Strings applied to the file names, where each line consists of
